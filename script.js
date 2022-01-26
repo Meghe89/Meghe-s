d0498e2056f8a32b10b23f743523dfd6259a0491
Michele Cozzola <cozzolamichele89@gmail.com>
2022-01-26 19:47:29 +0100

    menu toggle

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+/* DOM VARIABLES */
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu')
+const menuItems = document.querySelectorAll('.menu__items')
 
+/* FUNCTION TOGGLE MENU SMALL SCREEN */
 hamburger.addEventListener('click', ()=>{
     menu.classList.toggle('menu--open')
+    hamburger.classList.toggle('is-active')
+})
+
+/* FUNCTION CLOSE MENU SMALL ON MENU ITEMS CLICK */
+menuItems.forEach(el=>{
+    el.addEventListener('click', () =>{
+         menu.classList.remove('menu--open')
+    })
 })
