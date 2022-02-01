d83ee3c4cb9a68afa8f2f1400b1f83db0eb91443
Michele Cozzola <cozzolamichele89@gmail.com>
2022-02-01 19:24:48 +0100

    slider

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu')
 const menuItems = document.querySelectorAll('.menu__items')
 
+const nav = document.querySelector('.nav');
+const link = document.querySelectorAll('.menu__items')
+
 /* FUNCTION TOGGLE MENU SMALL SCREEN */
 hamburger.addEventListener('click', ()=>{
     menu.classList.toggle('menu--open')
@@ -15,3 +18,18 @@ menuItems.forEach(el=>{
          menu.classList.remove('menu--open')
     })
 })
+/* 
+window.addEventListener('scroll',()=>{
+    if(scrollY > 200){       
+        nav.classList.add('bg-nav')
+        link.forEach(el=>{
+            el.classList.add('link-black')
+        })
+    }else{
+        nav.classList.remove('bg-nav')
+        link.forEach(el=>{
+            el.classList.remove('link-black')
+        })
+    }
+}) */
+
